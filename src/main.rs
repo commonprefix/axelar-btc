8f1ca42b20d7148a896046c2e3c46c8a1f97ba5c
Orfeas Stefanos Thyfronitis Litos <orfeas.litos@hotmail.com>
2024-05-23 14:15:10 +0100

    Reduce weight to single party for tests

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,7 @@ fn main() {
 
     script = script
         .push_int(
-            (1..=COMMITTEE_SIZE).sum::<usize>().try_into().unwrap()
+            1//(1..=COMMITTEE_SIZE).sum::<usize>().try_into().unwrap()
         )
         .push_opcode(OP_GREATERTHAN);
 
