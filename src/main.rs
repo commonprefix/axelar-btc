34ae8f9027ec7c72085f4a91c656291192a48b8b
Orfeas Stefanos Thyfronitis Litos <orfeas.litos@hotmail.com>
2024-05-23 13:57:41 +0100

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
     // First time
     // $ bitcoin-core.cli -rpcport=18443 -rpcpassword=1234 -regtest -named createwallet wallet_name=default load_on_startup=true
     // $ bitcoin-core.cli -rpcport=18443 -rpcpassword=1234 -regtest getnewaddress
-    // bitcoin-core.cli  -rpcport=18443 -rpcpassword=1234 -regtest generatetoaddress 101 <previous_output>
+    // $ bitcoin-core.cli  -rpcport=18443 -rpcpassword=1234 -regtest generatetoaddress 101 <previous_output>
     const COMMITTEE_SIZE: usize = 4;
 
     const NETWORK: Network = Network::Regtest;
